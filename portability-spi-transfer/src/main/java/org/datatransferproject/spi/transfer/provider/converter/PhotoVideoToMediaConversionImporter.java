cadeeb3eeb63427e256f9d592376a9ba7647457d
Jonathan Zacsh <j@zac.sh>
2022-06-01 13:42:11 -0500

    nit: slight cleanup and javadoc on WIP code

--- portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/provider/converter/PhotoVideoToMediaConversionImporter.java
+++ portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/provider/converter/PhotoVideoToMediaConversionImporter.java
@@ -7,27 +7,49 @@ import org.datatransferproject.spi.transfer.provider.Importer;
 import org.datatransferproject.types.common.models.DataModel;
 import org.datatransferproject.types.common.models.media.MediaContainerResource;
 import org.datatransferproject.types.common.models.photos.PhotosContainerResource;
+import org.datatransferproject.types.common.models.videos.VideoContainerResource;
 import org.datatransferproject.types.transfer.auth.AuthData;
 
-public class PhotoVideoToMediaConversionImporter<
-    A extends AuthData, PCR extends PhotosContainerResource, VCR extends VideoContainerResource, PI extends Importer<A, PCR>, VI extends Importer<A, VCR>, >
-    implements Importer<A, MediaContainerResource> {
-  private final PI wrappedPhotoImporter;
-  private final VI wrappedVideoImporter;
 
-  public PhotoToMediaConverionImporter(PI wrappedPhotoImporter, VI wrappedVideoImporter) {
+// TODO(WIP) audit javadoc on _all_ the converter package to follow this javadoc as a template.
+/**
+ * Automatically produces a "MEDIA" importer from more basic "PHOTOS" and "VIDEO" importers.
+ *
+ * Produces an `Importer` that can handle import jobs for `MediaContainerResource` (aka "MEDIA"
+ * jobs) given two existing importers: one that can handle the specific jobs of importing
+ * `PhotosContainerResource` (aka "PHOTOS" jobs) and a second that can handle
+ * `VideoContainerResource` `MediaContainerResource` (aka "VIDEO" jobs).
+ *
+ * This is intended for providers who do not support "MEDIA" as a special case.
+ */
+// TODO(WIP) fix the primitives-obession causing us to key Providers on "PHOTOS" string rather
+// than underlying file types.
+public class PhotoVideoToMediaConversionImporter<
+    A extends AuthData,
+    PCR extends PhotosContainerResource,
+    VCR extends VideoContainerResource,
+    WrappedPhotoImporter extends Importer<A, PCR>,
+    WrappedVideoImporter extends Importer<A, VCR>,
+    > implements Importer<A, MediaContainerResource> {
+  private final WrappedPhotoImporter wrappedPhotoImporter;
+  private final WrappedVideoImporter wrappedVideoImporter;
+
+  public PhotoToMediaConverionImporter(
+      WrappedPhotoImporter wrappedPhotoImporter,
+      WrappedVideoImporter wrappedVideoImporter) {
     this.wrappedPhotoImporter = wrappedPhotoImporter;
     this.wrappedVideoImporter = wrappedVideoImporter;
   }
 
-  ImportResult importItem(UUID jobId, IdempotentImportExecutor idempotentExecutor, A authData,
+  ImportResult importItem(
+      UUID jobId,
+      IdempotentImportExecutor idempotentExecutor,
+      A authData,
       MediaContainerResource data) throws Exception {
-    VideoContainerResource
-        generatedVideoContainer; // TODO(DO NOT MERGE) complete this using the existing utils
-    PhotosContainerResource
-        generatedPhotoContainer; // TODO(DO NOT MERGE) complete this using the existing utils
+    VCR generatedVideoContainer; // TODO(DO NOT MERGE) complete this using the existing utils
+    PCR generatedPhotoContainer; // TODO(DO NOT MERGE) complete this using the existing utils
 
-    wrappedVideoImporter.importItem(jobId, idempotentexecutor, authData, generatedVideoContainer);
+    wrappedPhotoImporter.importItem(jobId, idempotentexecutor, authData, generatedVideoContainer);
     wrappedVideoImporter.importItem(jobId, idempotentexecutor, authData, generatedPhotoContainer);
   }
 }
